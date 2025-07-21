83892a52ec134639a1951469c21c36ea318b3afe
dhanq <dhanquijano6@gmail.com>
2025-07-21 20:29:59 +0800

    removed login requirement and fixed header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,13 +2,11 @@ import React from "react";
 
 const Page = () => {
   return (
-    <div className="hero-bg">
+    <div className="hero-bg mx-auto max-w-screen-xl px-4">
       <div className="text-6xl text-white font-semibold">
         Greetings from the Heart!
       </div>
-
     </div>
   );
 };
 export default Page;
-
